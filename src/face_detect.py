35ce07deea78474cf4d2e7fad69538a0f788befe
mohithm2 <33551252+mohithm2@users.noreply.github.com>
2019-09-21 19:18:29 +0530

    Added face detect to Videos and OnCamera

--- src/face_detect.py
+++ src/face_detect.py
@@ -1,16 +1,40 @@
 import numpy as np
 from cv2 import cv2
 
-def detect_function():
+def image_detect():
 
-    face_cascade = cv2.CascadeClassifier('src/hf.xml')   #running the pretrained classifier
-    img = cv2.imread('media/images/sachin.jpg') #reading the image
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #converting it to grayscale
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5) #recognizing the face coordiantes in the image using the classifer
-    for (x,y,w,h) in faces:
-        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #highlighting the face with a rectangle
-        
-
-    cv2.imshow('img',img) # displaying the image with a rectangle 
+    img = cv2.imread('media/images/sachin.jpg') 
+    detect_faces(img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def video_detect():
+
+    cap = cv2.VideoCapture('media/videos/1.MP4')
+    while(cap.isOpened()):
+        ret,frame = cap.read()
+        detect_faces(frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def cam_detect():
+    cap = cv2.VideoCapture(0)
+    while(True):
+        ret, frame = cap.read()
+        detect_faces(frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def detect_faces(frame):
+    face_cascade = cv2.CascadeClassifier('src/hf.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        gray = cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.imshow('frame',gray)
--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
-from face_detect import detect_function
-
-detect_function() #calling function to recognize the face in the image
+from face_detect import image_detect,video_detect,cam_detect
+import sys
+    
+if sys.argv[0] == 1:
+    image_detect()
+elif sys.argv[0] == 2:
+    video_detect()
+else:
+    cam_detect()
